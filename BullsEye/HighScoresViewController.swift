11829010647cf55646e08e4bf059ee6dc215636d
Amr Hesham <amrhesham9595@gmail.com>
2020-12-26 12:23:41 +0200

    adding high score controller and it's proto type cell

---
+++ BullsEye/HighScoresViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HighScoresViewController.swift
+//  BullsEye
+//
+//  Created by Amr Hesham on 26/12/2020.
+//  Copyright © 2020 Amr Hesham. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - HighScoresViewController
+//
+class HighScoresViewController: UITableViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
